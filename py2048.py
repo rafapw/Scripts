fbff59f1579dd991cb50d9c76dcf5a59a0735f1a
rafapw <37386328+rafapw@users.noreply.github.com>
2021-02-09 12:28:34 -0300

    Update py2048.py

--- py2048.py
+++ py2048.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 from datetime import datetime
 import time
@@ -44,9 +43,6 @@ def calcs(driver,Keys):
         except:
             pass
 
-        #print(tile_map)
-        #print(tile_ant)
-        
         if tile_map!=tile_ant:
             driver.find_element_by_css_selector('body').send_keys(directions[0])
             tile_ant=tile_map
@@ -69,35 +65,4 @@ def calcs(driver,Keys):
 
         n=0
 
-def depois():
-    input()
-    for c in range(1,5):
-        for r in range(1,5):
-            try:
-
-                print(driver.find_element_by_css_selector('div.grid-row:nth-child('+str(c)+') > div:nth-child('+str(r)+')').get_attribute('innerHTML'))
-                
-                #driver.find_element_by_class_name('tile tile-'+str(n)+' tile-position-'+str(c)+'-'+str(r))
-                #print(str(c)+'-'+str(r)+': '+str(n))
-            #except Exception as e:
-                #print(e)
-                #pass
-
-            #try:
-                #driver.find_element_by_class_name('tile tile-'+str(n)+' tile-position-'+str(c)+'-'+str(r)+' tile-new')
-                #print(str(c)+'-'+str(r)+': '+str(n))
-            #except Exception as e:
-                #print(e)
-                #pass
-
-            #try:
-                #driver.find_element_by_class_name('tile tile-'+str(n)+' tile-position-'+str(c)+'-'+str(r)+' tile-merged')
-                #print(str(c)+'-'+str(r)+': '+str(n))
-            #except Exception as e:
-                #print(e)
-                #pass
-            except:
-                pass
-
-        #n*=2
 navigate()
